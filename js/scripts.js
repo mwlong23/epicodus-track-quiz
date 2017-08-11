0791a8ee757a5433e5ce88a30d967a4aab68d2a4
Mitch Long <mwlong23@gmail.com>
2017-08-11 09:26:35 -0700

    add radio button questions and establish form submit functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
-$(document).ready(function)({
-  $("").
+$(document).ready(function(){
+  $("form#quiz").submit(function(event){
+    event.preventDefault();
+
+    var answer1 = $("#question1").val();
+    // alert(answer1 + answer2 + answer3  + answer5 + "    -" +"made it past answer1");
+    var answer2 = $("#question2").val();
+    // alert(answer1 + answer2 + answer3 +  answer5 + "    -" +"made it past answer1");
+    var answer3 = $("#question3").val();
+    // alert(answer1 + answer2 + answer3 +  answer5 + "    -" +"made it past answer1");
+    var answer4 = $("input:radio[name=answer4]:checked").val();
+    var dob = $("#born").val();
+    alert(answer1 + answer2 + answer3 + dob + answer4 + "    -" +"made it past answer1");
+
+  });
 });
