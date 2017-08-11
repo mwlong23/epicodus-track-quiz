de4567b78172c1eecadff5230e219ccc3aecd0d9
Mitch Long <mwlong23@gmail.com>
2017-08-11 09:49:11 -0700

    add content to questionair

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
     var answer3 = $("#question3").val();
     // alert(answer1 + answer2 + answer3 +  answer5 + "    -" +"made it past answer1");
     var answer4 = $("input:radio[name=answer4]:checked").val();
-    var dob = $("#born").val();
+    var answer4 = $("#startDate").val();
     alert(answer1 + answer2 + answer3 + dob + answer4 + "    -" +"made it past answer1");
 
   });
