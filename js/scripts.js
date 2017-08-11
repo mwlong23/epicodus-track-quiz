9ab455789f5befe32949e6c19ecd58e102d9c9b7
Mitch Long <mwlong23@gmail.com>
2017-08-11 10:11:50 -0700

    add functoinality to hide quiz when completed.

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,19 @@ $(document).ready(function(){
     event.preventDefault();
 
     var answer1 = $("#question1").val();
-    // alert(answer1 + answer2 + answer3  + answer5 + "    -" +"made it past answer1");
     var answer2 = $("#question2").val();
-    // alert(answer1 + answer2 + answer3 +  answer5 + "    -" +"made it past answer1");
     var answer3 = $("#question3").val();
-    // alert(answer1 + answer2 + answer3 +  answer5 + "    -" +"made it past answer1");
     var answer4 = $("input:radio[name=answer4]:checked").val();
-    var answer4 = $("#startDate").val();
-    alert(answer1 + answer2 + answer3 + dob + answer4 + "    -" +"made it past answer1");
+    var answer5 = $("#startDate").val();
+    alert(answer1 + answer2 + answer3 + answer5 + answer4 + "    -" +"made it past answer1");
+
+    if (answer1 === "" || answer2 === "" || answer3 === ""){
+      alert("You are missing one or more answers, please answer all questions!");
+    } else {
+      $("#result5").show();
+    }
+
+    $("form#quiz").hide();
 
   });
 });
